8ffa472590b7c750c287337e07b2bb72108392c1
Victor <victor.carrillo.2112@hotmail.com>
2020-10-25 10:47:16 -0600

    WIP FirstRelease

--- HO2/webapp/WEB-INF/classes/victor/OperationsServlet.java
+++ HO2/webapp/WEB-INF/classes/victor/OperationsServlet.java
@@ -10,9 +10,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 public final class OperationsServlet extends HttpServlet {
+    private HashMap<String, HttpOperationController> availableOperations;
     public OperationsServlet() {
+        availableOperations = new HashMap<String, HttpOperationController>();
+        availableOperations.put("squared-root", new SquaredRoot());
+        availableOperations.put("power", new Power());
+        availableOperations.put("euler", new Euler());
+        availableOperations.put("fibonacci", new Fibonacci());
+        availableOperations.put("random-numbers", new RandomNumbers());
+        availableOperations.put("prime", new Prime());
+        availableOperations.put("division", new Division());
+        availableOperations.put("binary", new Binary());
+
+
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -37,5 +50,76 @@ public final class OperationsServlet extends HttpServlet {
         writer.println("</table>");
         writer.println("</body>");
         writer.println("</html>");
+    }
+}
+/*
+*
+* response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html><html>");
+        out.println("<head>");
+        out.println("<meta charset=\"UTF-8\" />");*/
+abstract class HttpOperationController {
+    protected HttpServletRequest request;
+    protected HttpServletResponse response;
+    protected PrintWriter board;
+
+    public void start(HttpServletRequest request, HttpServletResponse response, PrintWriter board){
+        this.response = response;
+        this.request = request;
+        this.board = board;
+        doWork();
+    }
+
+    public void print(String toPrint){
+        out.println(toPrint)
+    }
+
+    
+
+    public abstract void doWork() throws IOException, ServletException;
+}
+
+
+class SquaredRoot extends HttpOperationController {
+    public void doWork() throws IOException, ServletException {
+
+    }
+}
+class Power extends HttpOperationController {
+    public void doWork() throws IOException, ServletException {
+
+    }
+}
+class Euler extends HttpOperationController {
+    public void doWork() throws IOException, ServletException {
+
+    }
+}
+class Fibonacci extends HttpOperationController {
+    public void doWork() throws IOException, ServletException {
+
+    }
+}
+class RandomNumbers extends HttpOperationController {
+    public void doWork() throws IOException, ServletException {
+
+    }
+}
+class Prime extends HttpOperationController {
+    public void doWork() throws IOException, ServletException {
+
+    }
+}
+class Division extends HttpOperationController {
+    public void doWork() throws IOException, ServletException {
+
+    }
+}
+class Binary extends HttpOperationController {
+    public void doWork() throws IOException, ServletException {
+
     }
 }
